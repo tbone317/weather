763b6f0edcf2dacd7dd0154aa2d435ace3893db6
Travis Dunbar <tervis7@protonmail.com>
2022-04-29 03:06:35 -0500

    updated to run on heroku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const hbs = require("hbs", "");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //define paths for express config
 const publicDirPath = path.join(__dirname, "../public");
@@ -81,6 +82,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is up on 3000");
 });
